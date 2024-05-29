45835824375c63131f649ecc61dd40abb2b76f93
LynnAkinyi <78434875+LynnAkinyi@users.noreply.github.com>
2024-05-29 14:02:40 +0300

    third commit

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -2,4 +2,7 @@ fun main() {
     println("Hello World!")
     println("I am learning Kotlin.")
     println("It is awesome!")
+    println(3 + 3)
+    print("I am learning Kotlin. ")
+    print("It is awesome!")
 }
